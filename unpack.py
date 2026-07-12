86b180e9cf33069179ffa4f4ef92d23ae4c221aa
ajmaldevs <ajumusthafa1779@gmail.com>
2026-07-12 06:40:16 +0000

    Jul 12, 2026, 12:10 PM

--- unpack.py
+++ unpack.py
@@ -2,5 +2,5 @@ def total(galleons,sickles,knuts):
     return ((galleons* 17+sickles)* 29+knuts)
 
 
-coins=[100,23,40],[123,342,33]
-print(total(*coins),'knuts')
+coins={"galleons":100,"sickles":50,"knuts":25}
+print(coins,'knuts')
