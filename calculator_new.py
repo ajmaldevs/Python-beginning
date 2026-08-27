a6fe52adfcc26f30b28fe9876a764ea2fdddadba
ajmaldevs <ajumusthafa1779@gmail.com>
2026-08-27 15:53:53 +0000

    Aug 27, 2026, 9:23 PM

--- calculator_new.py
+++ calculator_new.py
@@ -3,41 +3,52 @@ import sys
 from datetime import date
 
 try:
-    num_1 = int(input("Enter a number: "))
-    num_2 = int(input("Enter another number: "))
-    operator = input("Enter the operator: ")
-    print("Calculating.......")
-    time.sleep(2)
-    print("Result: ")
-    match operator:
-        case "+":
-            print(f"Addition = {num_1+num_2}")
-        case "-":
-            print(f"Substration = {num_1-num_2}")
-        case "*":
-            print(f"Multiplication = {num_1*num_2}")
-        case "/":
-            print(f"Division = {num_1/num_2}")
-        case "**":
-            print(f"Power = {num_1**num_2}")
-        case "//":
-            print(f"Floor Division = {num_1//num_2}")
-        case _:
-            print(f"No Operator Entered....")
-            sys.exit()
-
-    today = date.today()
-
-    with open("History.txt", "a") as file:
-        file.write(f"\n{today} : {num_1} {operator} {num_2}\n")
-
-    option = input("Do you wanna see history Y/N: ")
-    with open("History.txt","r") as file:
-        if option == "Y":
-            print("------HISTORY------")
-            print(file.read().strip())
+    def takeinput():
+        num_1 = int(input("Enter a number: "))
+        num_2 = int(input("Enter another number: "))
+        print("Operators : + , - , * , / , ** , //")
+        operator = input("Enter the operator: ")
+        return num_1,num_2,operator
+    
+    def main():
+        num_1,num_2,operator=takeinput()
+        option=input("Do you Wanna Change Numbers ? Y/N").lower()
+        if option=="y":
+                takeinput()
         else:
-            sys.exit()
+                pass
+        print("Calculating.......")
+        time.sleep(2)
+        print("Result: ")
+        match operator:
+            case "+":
+                print(f"Addition = {num_1+num_2}")
+            case "-":
+                print(f"Substration = {num_1-num_2}")
+            case "*":
+                print(f"Multiplication = {num_1*num_2}")
+            case "/":
+                print(f"Division = {num_1/num_2}")
+            case "**":
+                print(f"Power = {num_1**num_2}")
+            case "//":
+                print(f"Floor Division = {num_1//num_2}")
+            case _:
+                print(f"No Operator Entered....")
+                sys.exit()
+
+        today = date.today()
+
+        with open("History.txt", "a") as file:
+            file.write(f"\n{today} : {num_1} {operator} {num_2}")
+
+        options = input("Do you wanna see history Y/N: ").lower()
+        with open("History.txt","r") as file:
+            if options == "y":
+                print("------HISTORY------")
+                print(file.read().strip())
+            else:
+                sys.exit()
 
 except ValueError:
     print("Value Error")
