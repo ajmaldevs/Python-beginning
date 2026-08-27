a79adb17bb0081ed21db72a42cd4bddeb521f8ec
ajmaldevs <ajumusthafa1779@gmail.com>
2026-08-27 19:12:16 +0530

    last

---
+++ slicing.py
@@ -0,0 +1,5 @@
+text=input("Enter a Word: ")
+print(text[3:])
+#here the text is sliced by this line by which the word is printed from the 3rd place
+#we could also give the end place like
+print(text[2:6])
