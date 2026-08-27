5d1a3d6f9679a75319f2b47b6791cb7888dd2216
ajmaldevs <ajumusthafa1779@gmail.com>
2026-08-27 14:47:42 +0000

    Aug 27, 2026, 8:17 PM

--- chatgpt.py
+++
@@ -1,9 +0,0 @@
-from openai import OpenAI
-client=OpenAI(api_key="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")
-
-response=client.responses.create(
-    input="What is CS50x?",
-    model="gpt-4o-mini"
-)
-
-print(response.output_text)
