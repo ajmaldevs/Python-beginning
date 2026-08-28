d12652eb04debda66082d527b67315a4df80d5f9
ajmaldevs <ajumusthafa1779@gmail.com>
2026-08-28 13:51:53 +0000

    Aug 28, 2026, 7:21 PM

--- time.py
+++ time.py
@@ -1,6 +1,22 @@
 import time
+print("Welcome to Typing Speed Tester..........")
+print("beginning.....")
+time.sleep(2)
 start_time=time.time()
 variable=input("Enter the Sentence: ")
 end_time=time.time()
 time_taken=(end_time-start_time)
-print(f"{time_taken:.2f}")
+count=0
+for i in variable:
+    count=count+1
+
+wc=1
+for j in variable:
+    if " " in variable:
+        wc=wc+1
+
+print(f"✨Number of Characters: {count}")
+time.sleep(0.25)
+print(f"🌐Word Count: {wc}")
+time.sleep(0.25)
+print(f"⏱️Time Taken :{time_taken:.2f} Seconds")
