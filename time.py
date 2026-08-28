cb7348c211eaaae7719d51afcdd2711daf155c38
ajmaldevs <ajumusthafa1779@gmail.com>
2026-08-28 13:44:08 +0000

    Aug 28, 2026, 7:14 PM

--- time.py
+++ time.py
@@ -2,5 +2,5 @@ import time
 start_time=time.time()
 variable=input("Enter the Sentence: ")
 end_time=time.time()
-time_taken=(end_time-start_time)/60
+time_taken=(end_time-start_time)
 print(f"{time_taken:.2f}")
